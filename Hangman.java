9c86a18123c9336a19337d70a6e42a9d329918d8
CS106A <>
2019-02-22 03:11:17 -0800

    {"types":"C","runs":63}

--- Hangman.java
+++ Hangman.java
@@ -79,7 +79,7 @@ public class Hangman extends ConsoleProgram {
  	private void pickWord() {
  		Scanner lexicon = new Scanner(new File("HangmanLexicon.txt"));
  		int rg = rgen.nextInt(0, lexicon.getWordCount() - 1);
- 		word = lexicon.getWord(index);
+ 		word = lexicon.getWord();
  	}			
  	
 	private void setUp() {
