adf09290f38512a7bd371783d4892618351dd7b7
CS106A <>
2019-02-22 10:38:04 -0800

    {"types":"C","runs":64}

--- Hangman.java
+++ Hangman.java
@@ -55,7 +55,7 @@ public class Hangman extends ConsoleProgram {
 	 ***********************************************************/
 	
 	private int guesses = N_GUESSES;
-	private String word;
+	private String word = pickWord();
 	private String hiddenWord;
 	private String getLetter;
 	private ArrayList<GLine> ropes = new ArrayList<GLine>();
